163d5c967eeb9f0ab32f4aa8cd65592adf974506
sohseyedi <sohseyedi@gmail.com>
2024-03-10 17:00:01 +0330

    formate date function

---
+++ src/utils/toLocalDate.js
@@ -0,0 +1,5 @@
+function toLocalDate(date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = new Date(date).toLocaleDateString('en-US', options);
+    return formattedDate;
+  }
