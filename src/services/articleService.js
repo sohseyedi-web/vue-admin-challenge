9c6eb690b6d6a45c455aa2d33ef366307b6a220d
sohseyedi <sohseyedi@gmail.com>
2024-03-11 18:43:06 +0330

    article service

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -4,4 +4,11 @@ export function getArticles() {
     return http.get("/articles").then((res) => res.data);
   }
 
+export function createArticles({article}){
+  return http.post("/articles", {article}).then((res) => res.data)
+}
+export function editArticle({article,slug}){
+  return http.put(`/articles/${slug}`,{article}).then((res) => res.data)
+}
+
   
