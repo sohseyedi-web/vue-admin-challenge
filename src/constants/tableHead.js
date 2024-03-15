6fd27910c1cbe0de2199d64bdb84fc07e3af30a9
sohseyedi <sohseyedi@gmail.com>
2024-03-15 11:54:30 +0330

    fixed bug table heads

--- src/constants/tableHead.js
+++ src/constants/tableHead.js
@@ -1,8 +1,8 @@
 export const tableHeadItems = [
-    {id : 1 , label : "#"},
-    {id : 2 , label : "Title"},
-    {id : 3 , label : "Author"},
-    {id : 4 , label : "Tags"},
-    {id : 5 , label : "Excerpt"},
-    {id : 6 , label : "Created"},
-]
+  { id: 1, label: "#" },
+  { id: 2, label: "Title" },
+  { id: 3, label: "Author" },
+  { id: 4, label: "Tags" },
+  { id: 5, label: "Excerpt" },
+  { id: 6, label: "Created" },
+];
