18eb93eacf68985ae655a74270b13a6bec01c0f4
sohseyedi <sohseyedi@gmail.com>
2024-03-15 19:50:04 +0330

    table bug fixed

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -21,22 +21,22 @@
         </thead>
         <!-- table body -->
         <tbody>
-          <tr v-for="(article, index) in paginatedData" :key="article.slug">
+          <tr v-for="(article, index) in articles" :key="article.slug">
             <TableBody :index="index" :article="article" />
           </tr>
         </tbody>
       </table>
     </div>
     <!--pagination  -->
-    <div class="mx-auto w-25" >
+    <div class="mx-auto w-25">
       <nav aria-label="Page navigation example" class="w-100">
         <ul class="pagination">
           <li class="page-item">
             <a
               class="page-link"
-              style="color: #373a3c;"
+              style="color: #373a3c"
               @click="previousPage"
-              :disabled="parseInt(this.$route.params.page) === 1"
+              :disabled="currentPage === 1"
               aria-label="Previous"
             >
               <span aria-hidden="true">&#60;</span>
@@ -46,20 +46,25 @@
           <li
             class="page-item"
             :class="{
-              'active text-warning': parseInt(this.$route.params.page) === page,
+              'active text-warning': currentPage === page,
             }"
             v-for="page in visiblePages"
             :key="page"
           >
-            <a @click="changePage(page)" class="page-link" style="color: #373a3c;">{{ page }}</a>
+            <a
+              @click="changePage(page)"
+              class="page-link"
+              style="color: #373a3c"
+              >{{ page }}</a
+            >
           </li>
           <li class="page-item">
             <a
               class="page-link"
               aria-label="Next"
-              style="color: #373a3c;"
+              style="color: #373a3c"
               @click="nextPage"
-              :disabled="parseInt(this.$route.params.page) === totalPages"
+              :disabled="currentPage === totalPages"
             >
               <span aria-hidden="true">&#62;</span>
               <span class="sr-only">Next</span>
@@ -101,10 +106,7 @@ export default {
     async getAllArticle() {
       this.isLoading = true;
       try {
-        const data = await getArticles(
-          this.itemsPerPage,
-          this.$route.params.page
-        );
+        const data = await getArticles(this.itemsPerPage, this.currentPage);
         this.articles = data.articles;
         this.articleLen = data.articlesCount;
       } catch (error) {
@@ -118,39 +120,40 @@ export default {
     },
     // change page with pagination functions
     changePage(page) {
-      this.$router.push({ name: "Home", params: { page: page } });
+      this.currentPage = page;
+      this.$router.push({ name: "Home", params: { page: this.currentPage } });
     },
     previousPage() {
+      this.currentPage--;
       if (parseInt(this.$route.params.page) > 1) {
         this.$router.push({
           name: "Home",
           params: { page: parseInt(this.$route.params.page) - 1 },
         });
       }
+      this.getAllArticle();
     },
     nextPage() {
       if (parseInt(this.$route.params.page) < this.totalPages) {
+        this.currentPage++;
         this.$router.push({
           name: "Home",
           params: { page: parseInt(this.$route.params.page) + 1 },
         });
+        this.getAllArticle();
       }
     },
   },
   computed: {
     // data pages
-    paginatedData() {
-      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-      const endIndex = startIndex + this.itemsPerPage;
-      return this.articles.slice(startIndex, endIndex);
-    },
+
     totalPages() {
       return Math.ceil(this.articleLen / this.itemsPerPage);
     },
     visiblePages() {
       const totalPages = this.totalPages;
-      let startPage = parseInt(this.$route.params.page);
-      let endPage = parseInt(this.$route.params.page) + 3;
+      let startPage = this.currentPage;
+      let endPage = startPage + 3;
 
       if (endPage > totalPages) {
         endPage = totalPages;
@@ -165,14 +168,6 @@ export default {
 
   // for change page and loading data
   watch: {
-    "$route.params.page": {
-      immediate: true,
-      handler(newValue, oldValue) {
-        if (newValue !== oldValue) {
-          this.getAllArticle();
-        }
-      },
-    },
     currentPage: {
       immediate: true,
       handler(newValue, oldValue) {
